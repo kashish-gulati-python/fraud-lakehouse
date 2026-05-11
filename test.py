281c885cbecf0b21b6506229abcd51d082343553
Kashish Gulati <kgulati100@gmail.com>
2026-05-11 21:27:16 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import signal
-import sys
-
-def handler(signum, frame):
-  print('Signal received, exiting gracefully...')
-  sys.exit(0)
-
-signal.signal(signal.SIGINT, handler)
-print('Press Ctrl+C to trigger signal')
